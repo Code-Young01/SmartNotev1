8678ca20a6bec9d7aa2b26594d56f1a0a5d77803
emiliopgarcia23 <emiliopgarcia23@gmail.com>
2023-09-25 12:49:11 -0600

    feat: add create and update To-Do

--- todo/todo.py
+++ todo/todo.py
@@ -1,3 +1,4 @@
+from turtle import title
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, template_rendered, url_for
 )
@@ -11,7 +12,7 @@ bp = Blueprint('todo', __name__, url_prefix='/todos')
 @login_required
 def index():
     bd, c = get_db()
-    c.execute('SELECT t.id, t.description, u.username, t.completed, t.created_at FROM todo t JOIN user u on t.created_by = u.id ORDER BY t.created_at desc')
+    c.execute('SELECT t.id, t.title, t.description, u.username, t.completed, t.created_at FROM todo t JOIN user u on t.created_by = u.id ORDER BY t.created_at desc')
     todos = c.fetchall()
 
     return render_template('todo/index.html', todos=todos)
@@ -19,9 +20,48 @@ def index():
 @bp.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
-    return ''
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
 
-@bp.route('/update', methods=['GET', 'POST'])
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+        elif not description:
+            error = 'Description is required'
+        elif not title and not description:
+            error = 'Title and description is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute('INSERT INTO todo (created_by, title, description, completed)'
+                      'values (%s, %s, %s, %s)', (g.user['id'], title, description, False))
+            db.commit()
+            return redirect(url_for('todo.index'))
+        
+    return render_template('todo/create.html')
+
+def get_todo(id):
+    db, c = get_db()
+    c.execute('SELECT t.id, t.title, t.description, t.completed, t.created_by, t.created_at, u.username '
+              'FROM todo t join user u on t.created_by = u.id WHERE t.id = %s', (id,))
+    todo = c.fetchone()
+
+    if todo is None:
+        abort(404, f"The To-Do {id} doesn't exist")
+
+    return todo
+
+@bp.route('/update/<int:id>', methods=['GET', 'POST'])
 @login_required
-def update():
+def update(id):
+    todo = get_todo(id)
+    return render_template('todo/update.html', todo=todo)
+
+@bp.route('/delete/<int:id>', methods=['POST'])
+@login_required
+def delete(id):
     return ''
